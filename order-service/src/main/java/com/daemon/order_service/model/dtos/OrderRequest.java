1cbba33ba34b96c0ed5f71b4b125b11d67510bf4
elias provincia <provinciaelias@gmail.com>
2024-05-12 18:12:06 -0500

    feat: models to orders

---
+++ order-service/src/main/java/com/daemon/order_service/model/dtos/OrderRequest.java
@@ -0,0 +1,18 @@
+package com.daemon.order_service.model.dtos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderRequest {
+    private List<OrderItemsRequest> orderItems;
+}
---
+++ order-service/src/main/java/com/daemon/order_service/model/entities/Order.java
@@ -0,0 +1,26 @@
+package com.daemon.order_service.model.entities;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<OrderItems> orderItems;
+}
