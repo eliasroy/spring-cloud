84dbce5b21457e5462c13fd765a50e9db8bbf743
elias provincia <provinciaelias@gmail.com>
2024-06-20 23:24:18 -0500

    add repository

---
+++ inventory-service/src/main/java/com/daemon/inventoryservice/repositorie/InventoryRepository.java
@@ -0,0 +1,13 @@
+package com.daemon.inventoryservice.repositorie;
+
+import com.daemon.inventoryservice.model.entities.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    Optional<Inventory> findBySku(String sku);
+
+    List<Inventory> findBySkuIn(List<String> skus);
+}
