df5f66dd0812d4b055d7eb05c080430b45d1db39
elias provincia <provinciaelias@gmail.com>
2024-06-22 17:41:25 -0500

    implement service get orders

--- order-service/src/main/java/com/daemon/order_service/controllers/OrderController.java
+++ order-service/src/main/java/com/daemon/order_service/controllers/OrderController.java
@@ -1,10 +1,13 @@
 package com.daemon.order_service.controllers;
 
 import com.daemon.order_service.model.dtos.OrderRequest;
+import com.daemon.order_service.model.dtos.OrderResponse;
 import com.daemon.order_service.services.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,5 +19,11 @@ public class OrderController {
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         this.orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<OrderResponse> findAllOrders() {
+        return this.orderService.getAllOrders();
     }
 }
